f890c7ac0837d2aa226928a35da7821b39fbcbd6
Rajeshwari Subramaniam <Subramaniam.Rajeshwari@in.bosch.com>
2022-06-15 13:22:52 +0530

    rename testcases

--- tests/test_color_codes.py
+++ tests/test_color_codes.py
@@ -7,17 +7,17 @@ from cable_color_codes.get_color_codes import (
 
 
 class TestCablesPair(TestCase):
-    def test_number_to_pair(self):
+    def test_get_color_from_pair_number(self):
         pair_number = 4
         expected_major_color = 'White'
         expected_minor_color = 'Brown'
         major_color, minor_color = get_color_from_pair_number(pair_number)
-        self.assertEqual(major_color, expected_major_color.lower())
-        self.assertEqual(minor_color, expected_minor_color.lower())
+        self.assertEqual(expected_major_color.lower(), major_color)
+        self.assertEqual(expected_minor_color.lower(), minor_color)
 
-    def test_pair_to_number(self):
+    def test_get_pair_number_from_color(self):
         major_color = 'Black'
         minor_color = 'Orange'
         expected_pair_number = 12
         pair_number = get_pair_number_from_color(major_color, minor_color)
-        self.assertEqual(pair_number, expected_pair_number)
+        self.assertEqual(expected_pair_number, pair_number)
